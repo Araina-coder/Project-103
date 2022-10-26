2a74d15d10d7bca2d1fca2a708fd6abcb62697b4
Araina-coder <83049345+Araina-coder@users.noreply.github.com>
2022-10-25 18:47:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -12,13 +12,34 @@ Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = '<img id="capture_image" src="' + data_uri + '">';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
     });
 }
 console.log("ml5 version" + ml5.version);
-ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/AQGXE-IA8/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ujgTj3eeU/model.json', modelLoaded);
 
 
 function modelLoaded() {
     console.log("Model is loaded");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+
+
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+
+
+    } else {
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed[3];
+
+
+    }
 }
